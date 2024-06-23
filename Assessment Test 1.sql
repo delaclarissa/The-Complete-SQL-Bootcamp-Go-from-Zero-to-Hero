332ff868927ba50e4a5d8c2e266051359543f2ae
Adeline Clarissa <72727740+delaclarissa@users.noreply.github.com>
2024-06-23 21:16:47 +0700

    Assessment Test 1.sql

--- Assessment Test 1.sql
+++ Assessment Test 1.sql
@@ -1,17 +1,17 @@
 /* Assessment Test 1 */
 
 /* 1.) Return the customer IDs of customers who have spent at least $110 with the staff member who has an ID of 2. */
--- SELECT customer_id, SUM(amount) AS total_amount FROM payment
--- WHERE staff_id = 2
--- GROUP BY customer_id, staff_id
--- HAVING SUM(amount) > 110
+SELECT customer_id, SUM(amount) AS total_amount FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id, staff_id
+HAVING SUM(amount) > 110
 
 /* 2.) How many films begin with the letter J? */
--- SELECT COUNT(*) FROM film
--- WHERE title LIKE 'J%'
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'J%'
 
 /* 3.) What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500? */
--- SELECT concat(first_name, ' ', last_name) AS name FROM customer
--- WHERE first_name LIKE 'E%' AND address_id < 500
--- ORDER BY customer_id DESC
--- LIMIT 1
+SELECT concat(first_name, ' ', last_name) AS name FROM customer
+WHERE first_name LIKE 'E%' AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1
