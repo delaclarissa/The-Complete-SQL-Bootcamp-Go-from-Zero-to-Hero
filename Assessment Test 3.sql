65494482c389956ba5c48baf2f99566b73c2a9f1
Adeline Clarissa <72727740+delaclarissa@users.noreply.github.com>
2024-06-23 21:14:08 +0700

    Add files via upload

---
+++ Assessment Test 3.sql
@@ -0,0 +1,38 @@
+/* Assessment Test 3 */
+
+-- CREATE DATABASE school
+
+-- CREATE TABLE teachers(
+-- 	teacher_id serial PRIMARY KEY,
+-- 	first_name VARCHAR (255) NOT NULL,
+-- 	last_name VARCHAR(255) NOT NULL,
+-- 	homeroom_number int,
+-- 	department VARCHAR(255),
+-- 	email TEXT UNIQUE,
+-- 	phone TEXT UNIQUE
+-- )
+
+-- CREATE TABLE students(
+-- 	student_id serial PRIMARY KEY,
+-- 	first_name VARCHAR (255) NOT NULL,
+-- 	last_name VARCHAR(255) NOT NULL,
+-- 	homeroom_number int,
+-- 	phone TEXT UNIQUE NOT NULL,
+-- 	email TEXT UNIQUE,
+-- 	graduation_year int
+-- )
+
+-- INSERT INTO students(
+-- 	first_name, last_name, phone, graduation_year, homeroom_number
+-- )
+-- VALUES(
+-- 	'Mark', 'Watney', '777-555-1234', 2035, 5
+-- )
+
+-- INSERT INTO teachers(
+-- 	first_name, last_name, homeroom_number, department, email, phone
+-- )
+-- VALUES(
+-- 	'Jonas', 'Salk', 5, 'Biology', '777-555-1234', 'jsalk@school.org'
+-- )
+
